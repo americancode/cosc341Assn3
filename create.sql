ce53aaed2beaaf68392dafa5ff75821b38d0d4e8
Nathaniel Churchill <natechurchill16@gmail.com>
2017-10-17 15:26:59 -0400

    Adding to Github

---
+++ create.sql
@@ -0,0 +1,19 @@
+/*
+* COSC 341  Assignment 3
+* This script makes the tables for the Company and populates them.
+*/
+
+create table employee (
+    fname varchar(25),
+    lname varchar(25),
+    ssn number(9),
+    sex char(1),
+    salary number(30),
+    super_ssn number(9),
+    dno number(1)
+);
+
+
+create table department (
+
+)
