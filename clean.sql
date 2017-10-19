0a1020f713111cdfd722b96e7dbda7faa63b815d
Nathaniel Churchill <natechurchill16@gmail.com>
2017-10-18 21:05:52 -0400

    fixing the constraints (#2)

--- clean.sql
+++ clean.sql
@@ -1,4 +1,20 @@
 --Drop Assignment 3 tables
+alter table employee
+    drop constraint fk_1
+    drop constraint fk_2;
+
+alter table department
+    drop constraint fk_3;
+
+alter table dept_locations
+    drop constraint fk_4;
+
+alter table project
+    drop constraint fk_5;
+
+alter table works_on
+    drop constraint fk_6
+    drop constraint fk_7;
 
 drop table employee;
 drop table project;
